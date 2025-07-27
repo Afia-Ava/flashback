3ce38895e1c58cdcabc0c7b4c04bf8219db64960
Afia Ava <afiakhanomava@gmail.com>
2025-07-27 12:40:47 -0400

    added authentication + user profiles

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBBQUfzGoqkMF9M5L8YrksXeUlWwMXfwao',
+  authDomain: 'flashback-map.firebaseapp.com',
+  projectId: 'flashback-map',
+  storageBucket: 'flashback-map.appspot.com',
+  messagingSenderId: '679587368912',
+  appId: '1:679587368912:web:demoappid',
+  measurementId: 'G-demoid',
+};
+
+firebase.initializeApp(firebaseConfig);
