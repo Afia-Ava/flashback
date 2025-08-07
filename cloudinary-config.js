e9a72d7a511e89299cfbaa6817b992bfe42b454c
Afia Ava <afiakhanomava@gmail.com>
2025-08-07 05:19:19 -0400

    added cloudinary for image storing

---
+++ cloudinary-config.js
@@ -0,0 +1,29 @@
+function uploadToCloudinary(file) {
+  return new Promise(function (resolve, reject) {
+    const url = 'https://api.cloudinary.com/v1_1/dsflmbhkh/upload';
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', 'Flashback');
+    fetch(url, {
+      method: 'POST',
+      body: formData,
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data.secure_url) {
+          resolve(data.secure_url);
+        } else {
+          reject(
+            new Error(
+              data.error && data.error.message
+                ? data.error.message
+                : 'Upload failed'
+            )
+          );
+        }
+      })
+      .catch(reject);
+  });
+}
+
+window.uploadToCloudinary = uploadToCloudinary;
